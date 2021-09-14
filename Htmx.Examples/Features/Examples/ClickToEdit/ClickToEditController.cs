b833493cf00c994b7ee4a7973d3c1a5736826db6
Al Zaudtke <zaudtke@gmail.com>
2021-09-14 13:28:26 -0400

    anotehr refactor to sub feature

--- Htmx.Examples/Features/Examples/ClickToEdit/ClickToEditController.cs
+++ Htmx.Examples/Features/Examples/ClickToEdit/ClickToEditController.cs
@@ -2,38 +2,38 @@
 using Htmx.Examples.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Htmx.Examples.Features.Examples;
+namespace Htmx.Examples.Features.Examples.ClickToEdit;
 
-[Route("[controller]")]
-public class ExamplesController : Controller
+[Route("examples/click-to-edit")]
+public class ClickToEditController : Controller
 {
     private readonly ContactService _contactService;
 
-    public ExamplesController(ContactService service)
+    public ClickToEditController(ContactService service)
     {
         _contactService = service;
     }
 
-    [HttpGet, Route("click-to-edit")]
-    public async Task<IActionResult> ClickToEdit()
+    [HttpGet, Route("")]
+    public async Task<IActionResult> Index()
     {
         var contact = await _contactService.GetContactById(1);
         return Request.IsHtmx()
-            ? PartialView("Partials/_ContactCard", contact)
+            ? PartialView("_ContactCard", contact)
             : View(contact);
     }
 
-    [HttpGet, Route("click-to-edit/{id:int}/Edit")]
+    [HttpGet, Route("/{id:int}/Edit")]
     public async Task<IActionResult> EditContact(int id)
     {
         var contact = await _contactService.GetContactById(id);
-        return PartialView("Partials/_ContactForm", contact);
+        return PartialView("_ContactForm", contact);
     }
 
-    [HttpPost, Route("click-to-edit/{id:int}/Edit")]
+    [HttpPost, Route("/{id:int}/Edit")]
     public async Task<IActionResult> EditContact(int id, Contact contact)
     {
         var updated = await _contactService.UpdateContact(contact);
-        return RedirectToAction(nameof(ClickToEdit));
+        return RedirectToAction(nameof(Index));
     }
 }
