3d68ee9d296f94c6a841210ab599f3c0a904162e
Al Zaudtke <zaudtke@gmail.com>
2021-09-19 10:22:47 -0400

    delete row example

---
+++ Htmx.Examples/Features/Examples/DeleteRow/DeleteRowController.cs
@@ -0,0 +1,33 @@
+﻿using Htmx.Examples.Domain.Villains;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Htmx.Examples.Features.Examples.DeleteRow
+{
+
+    [Route("examples/delete-row")]
+    public class DeleteRowController : Controller
+    {
+
+        private readonly VillainService _villainService;
+
+        public DeleteRowController(VillainService villainService)
+        {
+            _villainService = villainService;
+        }
+
+        [HttpGet, Route("")]
+        public async Task<IActionResult> Index()
+        {
+            var villains = await _villainService.GetAll();
+            return View(villains);
+        }
+
+        [HttpPost, Route("{id:int}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            await _villainService.Delete(id);
+            return new EmptyResult();
+        }
+    }
+}
