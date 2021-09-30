665bf77764f8baf082f5d86664856deadbb41e84
Al Zaudtke <zaudtke@gmail.com>
2021-09-30 14:15:20 -0400

    infinite scroll example

--- Htmx.Examples/Features/Examples/InfiniteScroll/InfiniteScrollController.cs
+++ Htmx.Examples/Features/Examples/InfiniteScroll/InfiniteScrollController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Htmx.Examples.Features.Examples.InfiniteScroll;
 
 [Route("examples/infinite-scroll")]
 public class InfiniteScrollController : Controller
 {
+    private readonly IMediator _mediator;
+
+    public InfiniteScrollController(IMediator mediator) => _mediator = mediator;
+
+
     [HttpGet, Route("")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int page = 1)
     {
-        return View();
+        var result = await _mediator.Send(new ViewVillains.Query(page));
+        return Request.IsHtmx()
+            ? PartialView("_Rows", result)
+            : View(result);
     }
 }
---
+++ Htmx.Examples/Features/Examples/InfiniteScroll/ViewVillains.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Htmx.Examples.Features.Examples.InfiniteScroll;
+
+public class ViewVillains
+{
+    public record Query(int Page = 1) : IRequest<Result>;
+
+    public record Result(IEnumerable<Villain> Villains, string LoadMoreUrl = "");
+
+    public record Villain(int Id, string Name);
+
+    public class QueryHandler : IRequestHandler<Query, Result>
+    {
+        public Task<Result> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var start = ((request.Page - 1) * 20) + 1;
+            var list = Enumerable.Range(start, 20).Select(x => new Villain(x, $"Villain {x}"));
+            return Task.FromResult(new Result(list, $"/examples/infinite-scroll/?page={request.Page + 1}"));
+        }
+    }
+}
