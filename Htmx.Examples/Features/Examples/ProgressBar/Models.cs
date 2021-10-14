fb766f9316748f2399b2a4347b715f2155d8b98d
Al Zaudtke <zaudtke@gmail.com>
2021-10-14 15:11:29 -0400

    progress bar example

---
+++ Htmx.Examples/Features/Examples/ProgressBar/Models.cs
@@ -0,0 +1,2 @@
+namespace Htmx.Examples.Features.Examples.ProgressBar;
+public record ProgressBarModel(int Percentage, string HeaderMessage, string Trigger);
---
+++ Htmx.Examples/Features/Examples/ProgressBar/ProgressBarController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Htmx.Examples.Features.Examples.ProgressBar;
+
+[Route("examples/progress-bar")]
+public class ProgressBarController : Controller
+{
+    [HttpGet, Route("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost, Route("start")]
+    public IActionResult Start()
+    {
+        // Start some long running process
+        return RedirectToAction(nameof(Status), new {current = -1});
+    }
+
+    [HttpGet, Route("status/{current:int}")]
+    public IActionResult Status(int current)
+    {
+        var next = GetNextStatus(current);
+        ProgressBarModel model;
+
+        if(next >= 100)
+        {
+            model = new ProgressBarModel(next, "Complete", "none");
+        }
+        else
+        {
+            model = new ProgressBarModel(next, "Running", "load delay:600ms");
+        }
+
+        return PartialView("_ProgressBar", model);
+    }
+
+    private int GetNextStatus(int current)
+    {
+       return current switch
+       {
+           -1 => 0,
+           0 => 25,
+           25 => 29,
+           29 => 55,
+           55 => 66,
+           66 => 85,
+           85 => 98,
+           98 => 100,
+           _ => 0 // make compiler happy, cover all possibilities
+       };
+    }
+}
