b33a771a51b3d8dba97881463fb72fed8c581d81
Al Zaudtke <zaudtke@gmail.com>
2021-10-17 12:54:07 -0400

    machine switch

---
+++ Htmx.Examples/Features/Examples/Animations/AnimationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Htmx.Examples.Features.Examples.Animations;
+
+[Route("examples/animations")]
+public class AnimationsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
