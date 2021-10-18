13fc46e5333c829fa2353569c74bf47bf334400b
Al Zaudtke <zaudtke@gmail.com>
2021-10-18 12:05:35 -0400

    color throb

--- Htmx.Examples/Features/Examples/Animations/AnimationsController.cs
+++ Htmx.Examples/Features/Examples/Animations/AnimationsController.cs
@@ -5,8 +5,21 @@ namespace Htmx.Examples.Features.Examples.Animations;
 [Route("examples/animations")]
 public class AnimationsController : Controller
 {
+    [HttpGet, Route("")]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet, Route("colors")]
+    public IActionResult Colors(string current = "red")
+    {
+        var color = current switch
+        {
+            "red" => "blue",
+            "blue" => "green",
+            _ => "red"
+        };
+        return PartialView("_ColorThrob", color);
     }
 }
