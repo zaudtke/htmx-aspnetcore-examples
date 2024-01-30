9cbf27543942407bcaeaaad2d88e110466edfe6e
Al Zaudtke <zaudtke@gmail.com>
2024-01-30 09:03:15 -0600

    add razor pages project

---
+++ Htmx.RazorPages/Program.cs
@@ -0,0 +1,32 @@
+// Disable warnings for Expression Value is never used
+// Discard Characters in this file make it "ugly"
+#pragma warning disable IDE0058
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+// Enable warnings for Expression Value is never used
+#pragma warning restore IDE0058
